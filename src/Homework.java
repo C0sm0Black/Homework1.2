05ac3db23388d8030a45ecb0286ff4a5ce67bcab
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-12 23:02:32 +0200

    Task 8

--- src/Homework.java
+++ src/Homework.java
@@ -2,14 +2,11 @@ public class Homework {
 
     public static void main(String[] args) {
 
-        var weightBoxer1 = 78.2;
-        var weightBoxer2 = 82.7;
+        var timeWork = 640;
+        var timeWorksman = 8;
 
-        System.out.println("Общий вес 2-х боксеров = " + (weightBoxer1 + weightBoxer2));
-        System.out.println("Разница между весами боксеров = " + (-weightBoxer1 + weightBoxer2));
-
-        var weightDifference1 = -weightBoxer1 + weightBoxer2;
-        var weightDifference2 =  weightBoxer2 % weightBoxer1;
+        System.out.println("Всего работников в компании — " + (timeWork / timeWorksman) + " человек");
+        System.out.println("Если в компании работает " + ((timeWork / timeWorksman) + 94) + " человек, то всего " + (((timeWork / timeWorksman) + 94) * 8) + " часов работы может быть поделено между сотрудниками");
 
     }
 
