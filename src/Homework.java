d47afec4e17dc27b120819413f518a497330de11
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-12 22:56:29 +0200

    Task 4

--- src/Homework.java
+++ src/Homework.java
@@ -2,21 +2,14 @@ public class Homework {
 
     public static void main(String[] args) {
 
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
+        var friend = 19;
+        System.out.println("friend = " + friend);
 
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
+        friend = friend + 2;
+        System.out.println("friend = " + friend);
 
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-
-        System.out.println("dog = " + dog);
-        System.out.println("cat = " + cat);
-        System.out.println("paper = " + paper);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
 
     }
 
