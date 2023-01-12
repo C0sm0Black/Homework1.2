abfee60a384da28ed0ddc85ed4f97a4d84f745b0
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-12 22:57:54 +0200

    Task 6

--- src/Homework.java
+++ src/Homework.java
@@ -2,17 +2,11 @@ public class Homework {
 
     public static void main(String[] args) {
 
-        var frog = 3.5;
-        System.out.println("frog = " + frog);
+        var weightBoxer1 = 78.2;
+        var weightBoxer2 = 82.7;
 
-        frog = frog * 10;
-        System.out.println("frog = " + frog);
-
-        frog = frog / 3.5;
-        System.out.println("frog = " + frog);
-
-        frog = frog + 4;
-        System.out.println("frog = " + frog);
+        System.out.println("Общий вес 2-х боксеров = " + (weightBoxer1 + weightBoxer2));
+        System.out.println("Разница между весами боксеров = " + (-weightBoxer1 + weightBoxer2));
 
     }
 
